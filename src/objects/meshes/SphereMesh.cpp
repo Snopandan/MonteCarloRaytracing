31e38b0594e4959a7419d7ebee39b818ba452f65
Niklas <niklas@niklasandersson.net>
2015-10-01 17:00:12 +0200

    Add sphere mesh with intersection

---
+++ src/objects/meshes/SphereMesh.cpp
@@ -0,0 +1,41 @@
+#include "SphereMesh.h"
+
+SphereMesh::SphereMesh(glm::vec3 position, float radius)
+: position_(position), radius_(radius) {
+
+}
+
+SphereMesh::~SphereMesh() {
+
+}
+
+std::tuple<Mesh::Intersection, float, float> SphereMesh::getIntersections(Ray ray) {
+    glm::vec3 c = position_;
+    glm::vec3 o = ray.getOrigin();
+    glm::vec3 d = ray.getDirection();
+
+    float denominator = glm::dot(d, d);
+
+    glm::vec3 oMinusC = o - c;
+    float numeratorFirstPart = -1 * glm::dot(d, oMinusC);
+    float numeratorSecondPart = std::pow(glm::dot(oMinusC, d), 2) - glm::dot(d, d) * (glm::dot(oMinusC, oMinusC) - std::pow(radius_, 2));
+    float sMin = 0;
+    float sMax = 0;
+
+    if (numeratorSecondPart == 0.0) {
+      sMin = numeratorFirstPart / denominator;
+
+      return std::make_tuple(Mesh::Intersection::SINGLE_HIT, sMin, sMax);
+    }else if(numeratorSecondPart > 0) {
+      sMin = (numeratorFirstPart - std::sqrt(numeratorSecondPart)) / denominator;
+      sMax = (numeratorFirstPart + std::sqrt(numeratorSecondPart)) / denominator;
+
+      if (sMax < 0) {
+        return std::make_tuple(Mesh::Intersection::DOUBLE_HIT, sMax, sMin);
+      }else {
+        return std::make_tuple(Mesh::Intersection::DOUBLE_HIT, sMin, sMax);
+      }
+    }
+
+    return std::make_tuple(Mesh::Intersection::MISS, sMin, sMax);
+}
---
+++ src/objects/meshes/SphereMesh.h
@@ -0,0 +1,25 @@
+#ifndef SPHERE_MESH
+#define SPHERE_MESH
+
+#include <tuple>
+#include <cmath>
+#include <iostream>
+
+#include <glm/glm.hpp>
+
+#include "Mesh.h"
+#include "Ray.h"
+
+class SphereMesh : public Mesh{
+public:
+  SphereMesh(glm::vec3 position, float radius);
+  virtual ~SphereMesh();
+
+  virtual std::tuple<Mesh::Intersection, float, float> getIntersections(Ray ray);
+
+private:
+  glm::vec3 position_;
+  float radius_;
+};
+
+#endif
