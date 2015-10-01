c590f64628248df3b30dfda5988151b2c2d43bcb
EMPX <joade361@Student.liu.se>
2015-10-01 15:40:11 +0200

    Add ray tracing test of triangle

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 int main(const int argc, const char* argv[]) {
 
   Camera camera{glm::ivec2{800, 600},
-                glm::vec2{1, 1},
+                glm::vec2{0.1f, 0.1f},
                 glm::vec3{100, 50, 0}, 
                 100};
 
@@ -22,22 +22,24 @@ int main(const int argc, const char* argv[]) {
   std::vector<Ray*> rays = camera.getRays();
 
   Scene scene;
-  scene.add(new Plane());
+  scene.add(new Plane(glm::vec3(100,80,101), glm::vec3(100,30,101), glm::vec3(60,30,101)));
 
-  int val = 0;
-  for(auto& ray : rays) {
-    if( scene.intersect(ray) ) {
-      val = 255;
-    }
-  }
-  
 
-  unsigned width = 512, height = 512;
+  // int val = 0;
+  // for(auto& ray : rays) {
+  //   if( scene.intersect(ray) ) {
+  //     val = 255;
+  //   }
+  // }
+
+  unsigned width = camera.getPixels().x, height = camera.getPixels().y;
   std::vector<unsigned char> image;
   image.resize(width * height * 4);
-  for(unsigned y = 0; y < height; y++)
   for(unsigned x = 0; x < width; x++)
+  for(unsigned y = 0; y < height; y++)
   {
+    int val = scene.intersect(rays[width * y + x]) ? 255 : 0;
+
     image[4 * width * y + 4 * x + 0] = 0;
     image[4 * width * y + 4 * x + 1] = val;
     image[4 * width * y + 4 * x + 2] = 0;
--- src/objects/Plane.cpp
+++ src/objects/Plane.cpp
@@ -3,24 +3,78 @@
 
 Plane::Plane(const glm::vec3 upperLeftCorner, 
              const glm::vec3 lowerLeftCorner,
-             const glm::vec3 lowerRightCorner,
-             const glm::vec3 upperRightCorner)
+             const glm::vec3 lowerRightCorner)
 : upperLeftCorner_{upperLeftCorner}
 , lowerLeftCorner_{lowerLeftCorner}
 , lowerRightCorner_{lowerRightCorner}
-, upperRightCorner_{upperRightCorner} 
 { 
 
   glm::vec3 edge1 = glm::normalize(upperLeftCorner_ - lowerLeftCorner_);
   glm::vec3 edge2 = glm::normalize(lowerRightCorner_ - lowerLeftCorner_); 
 
   normal_ = glm::normalize(glm::cross(edge1, edge2));
+
 }
 
 
+
+#define EPSILON 0.000001
+
 bool Plane::intersect(Ray* ray) {
+  glm::vec3 V1 = upperLeftCorner_;
+  glm::vec3 V2 = lowerLeftCorner_;
+  glm::vec3 V3 = lowerRightCorner_;
+  glm::vec3 D = ray->getDirection();
+  glm::vec3 O = ray->getOrigin();
+  
+  float det, inv_det, u, v;
+  float t;
 
+  // Find vectors for two edges sharing V1
+  glm::vec3 e1 = V2 - V1;
+  glm::vec3 e2 = V3 - V1;
 
-  return true;
+  // Begin calculating determinant - also used to calculate u parameter
+  glm::vec3 P = glm::cross(D, e2);
+
+  // If determinant is near zero, ray lies in plane of triangle
+  det = glm::dot(e1, P);
+
+  // NOT CULLING
+  if(det > -EPSILON && det < EPSILON) {
+    return false;
+  }
+  inv_det = 1.0f / det;
+
+  // Calculate distance from V1 to ray origin
+  glm::vec3 T = O - V1;
+
+  // Calculate u parameter and test bound
+  u = glm::dot(T, P) * inv_det;
+
+  // The intersection lies outside of the triangle
+  if(u < 0.0f || u > 1.0f) {
+    return false;
+  }
+
+  // Prepare to test v parameter
+  glm::vec3 Q = glm::cross(T, e1);
+
+  // Calculate V parameter and test bound
+  v = glm::dot(D, Q) * inv_det;
+
+  // The intersection lies outside of the triangle
+  if(v < 0.0f || u + v  > 1.0f) {
+    return false;
+  }
+
+  t = glm::dot(e2, Q) * inv_det;
+
+  if(t > EPSILON) { //ray intersection
+    // *out = t;
+    return true;
+  }
+
+  return false;
 }
 
